d8914d205effa5310ce996428c5b404564df9ace
ooksushka <nuksu03@gmail.com>
2022-06-26 10:57:07 +0300

    L021 - Interface example

--- src/animals/Bird.java
+++ src/animals/Bird.java
@@ -1,18 +1,22 @@
 package animals;
 
-public class Bird extends Animal {
+public class Bird extends Animal implements Flight {
     protected Boolean flyModeActive = false;
 
     public Bird() {
         super(MoveType.FLY);
     }
 
-    public final void takeOff() {
-        flyModeActive = true;
+    @Override
+    public void takeOff() {
+       this. flyModeActive = true;
     }
-    public final void landing() {
-        flyModeActive = false;
+
+    @Override
+    public void landing() {
+        this.flyModeActive = false;
     }
+
     public void isTheBirdFlying() {
         System.out.println(
                 this.flyModeActive == true ? "Bird: flying mode - yes " : "Bird: flying mode - no"
